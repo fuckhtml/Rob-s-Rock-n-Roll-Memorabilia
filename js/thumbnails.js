26764ba7e6e21681848578c870b6e32633c66830
fuckhtml <jordenblacksnk@gmail.com>
2020-10-23 22:08:50 +0400

    Adds possibility to fetch data using a plain string, CSV, XML

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -2,10 +2,87 @@
 
 const initPage = () => {
 
-  const renderDetails = (itemID, description) => {
+  // for getDetails.php
+  const renderDetails = (itemID, description) => {    
     const detailsPane = document.querySelector('#detailsPane');
+
     detailsPane.querySelector('img').src = `images/${itemID}-detail.jpg`;
     detailsPane.querySelector('#description').innerHTML = `<p>${description}</p>`;
+  }
+
+  // for getDetailsCSV.php: comma seperated values
+  const renderDetailsCSV = (csv) => {
+    csv = csv.split(',');
+    const description = {
+      itemID: csv[0],
+      details: csv[1],
+      price: csv[2],
+      URLs: csv.slice(3)
+    }
+
+    const details = document.createElement('p');
+    details.innerHTML = `${description.details}`;
+
+    const price = document.createElement('p');
+    price.innerHTML = `<b>Price: ${description.price}<b>`;
+
+    const links = document.createElement('ul');
+    description.URLs.map(function(link) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = link;
+      a.innerText = link;
+      li.append(a);
+      links.append(li);
+    });
+
+    const detailsPane = document.querySelector('#detailsPane');
+    detailsPane.querySelector('img').src = `images/${description.itemID}-detail.jpg`;
+    detailsPane.querySelector('#description').innerHTML = '';
+    detailsPane.querySelector('#description').append(details);
+    detailsPane.querySelector('#description').append(price);
+    detailsPane.querySelector('#description').append(links);
+  }
+
+  // for getDetailsXML.php: comma seperated values
+  const renderDetailsXML = (xml) => {
+    // collect data
+    const data = {
+      itemID: xml.querySelector('item').id,
+      description: xml.querySelector('description').textContent,
+      price: xml.querySelector('price').textContent,
+      URLs: []
+    }
+    Array.from(xml.querySelector('resources').children).map(url => data.URLs.push(url.textContent));
+
+    // create DOM Element
+    const description = document.createElement('p');
+    description.textContent = `${data.description}`;
+
+    const price = document.createElement('p');
+    price.innerHTML = `<b>Price: ${data.price}<b>`;
+
+    const links = document.createElement('ul');
+    data.URLs.map(function(link) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = link;
+      a.innerText = link;
+      li.append(a);
+      links.append(li);
+    });
+
+    // add the DOM Element
+    const detailsPane = document.querySelector('#detailsPane');
+    detailsPane.querySelector('img').src = `images/${data.itemID}-detail.jpg`;
+    detailsPane.querySelector('#description').innerHTML = '';
+    detailsPane.querySelector('#description').append(description);
+    detailsPane.querySelector('#description').append(price);
+    detailsPane.querySelector('#description').append(links);
+  }
+
+  const renderDetailsXMLUpdated = (xml) => {
+    console.log(xml);
   }
 
   const thumbnailPaneOnClickHandler = (event) => {
@@ -15,10 +92,25 @@ const initPage = () => {
       let request = new XMLHttpRequest();
       request.addEventListener('readystatechange', function() {
         if (request.readyState === 4 && request.status === 200) {
-          renderDetails(itemID, request.responseText);
+          // renderDetails(itemID, request.responseText);
+          // renderDetailsCSV(request.responseText);
+          renderDetailsXML(request.responseXML);
+          // renderDetailsXMLUpdated(request.responseXML);
         }
       })
-      request.open('GET', `getDetails.php?ImageID=${itemID}`);
+
+      // getDetails.php | Regular String
+      // request.open('GET', `getDetails.php?ImageID=${itemID}`);
+
+      // getDetailsCSV.php | Regular String, but with inner logic
+      // request.open('GET', `getDetailsCSV.php?ImageID=${itemID}`);
+
+      // getDetailsXML.php | XML tree
+      request.open('GET', `getDetailsXML.php?ImageID=${itemID}`);
+
+      // getDetailsXml-updated.php | XML tree, but better structured
+      // request.open('GET', `getDetailsXML-updated.php?ImageID=${itemID}`);
+
       request.send();
     }
   }
